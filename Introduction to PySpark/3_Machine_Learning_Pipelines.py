c7c15a33e5fca41a6ff2c4396c6cac14fbf9ca11
Alan <luodante@gmail.com>
2019-11-15 20:46:34 -0500

    added pyspark file

---
+++ Introduction to PySpark/3_Machine_Learning_Pipelines.py
@@ -0,0 +1,16 @@
+# 1. Join the DataFrames
+
+# Rename year column
+planes = planes.withColumnRenamed("year", "plane_year")
+
+# Join the DataFrames
+model_data = flights.join(planes, on="tailnum", how="leftouter")
+
+## Python need numeric data to do machine learning
+# 2. String to integer
+# Cast the columns to integers
+model_data = model_data.withColumn("arr_delay", model_data.arr_delay.cast("integer"))
+model_data = model_data.withColumn("air_time", model_data.air_time.cast("integer"))
+model_data = model_data.withColumn("month", model_data.month.cast("integer"))
+model_data = model_data.withColumn("plane_year", model_data.plane_year.cast("integer"))
+
